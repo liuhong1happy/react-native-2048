c79b1204e244b14f550f418ef043bb3ab2433dea
Holly Liu <hollyliu@HollydeMac-mini.local>
2016-12-10 17:22:04 +0800

    upgrade android gradle & build version

--- android/app/src/main/java/com/reactnative2048/MainApplication.java
+++ android/app/src/main/java/com/reactnative2048/MainApplication.java
@@ -1,14 +1,11 @@
 package com.reactnative2048;
 
 import android.app.Application;
-import android.util.Log;
-
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +21,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+              new MainReactPackage()
       );
     }
   };
@@ -32,11 +29,5 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
   }
 }
